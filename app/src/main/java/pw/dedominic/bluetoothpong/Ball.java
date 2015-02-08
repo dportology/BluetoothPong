3ce4139b88380f5291e945f9f694baabb675ac58
adedomin <anthony@dedominic.pw>
2015-02-08 14:32:27 -0500

    added ball class

---
+++ app/src/main/java/pw/dedominic/bluetoothpong/Ball.java
@@ -0,0 +1,87 @@
+package pw.dedominic.bluetoothpong;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+/**
+ * Created by prussian on 2/8/15.
+ * The ping pong ball object
+ */
+public class Ball
+{
+    //defines ball size
+    private int radius;
+
+    // x and y coords
+    private double x;
+    private double y;
+
+    // amount balls move along x and y every frame
+    private double x_vel;
+    private double y_vel;
+
+    public Ball()
+    {
+        x = 0;
+        y = 0;
+        radius = 5;
+    }
+
+    // takes x y starting coords, likely screen width / 2 = x, screen height / 2 = y.
+    public Ball(double x_, double y_)
+    {
+        x = x_;
+        y = y_;
+        radius = 5;
+    }
+
+    // takes radius val too
+    public Ball(double x_, double y_, int rad)
+    {
+        x = x_;
+        y = y_;
+        radius = rad;
+    }
+
+    // to set velocity
+    public void setVel(double vel_x, double vel_y)
+    {
+        x_vel = vel_x;
+        y_vel = vel_y;
+    }
+
+    public double getX()
+    {
+        return x;
+    }
+
+    public double getY()
+    {
+        return y;
+    }
+
+    public int getRad()
+    {
+        return radius;
+    }
+
+    // when bounces off ceiling/floor
+    public void yDeflect()
+    {
+        y_vel = -y_vel;
+    }
+
+    // when bounces off paddle
+    public void xDeflect()
+    {
+        x_vel = -x_vel;
+    }
+
+    public void update(Canvas screen, Paint color)
+    {
+        x += x_vel;
+        y += y_vel;
+
+        screen.drawCircle((float)x, (float)y, radius, color);
+    }
+}
